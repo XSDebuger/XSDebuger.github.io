5b8af316d78f8162f94ab6e1cfe002043bbcf4ae
Zhu Jiamin <32239719+KXXH@users.noreply.github.com>
2020-02-26 23:27:44 +0800

    init

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  title: '不如去吃炸酱面',
+  description: '',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/KXXH',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+  },
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh', // 将会被设置为 <html> 的 lang 属性
+      title: '不如去吃炸酱面',
+      description: ''
+    },
+  }
+}
